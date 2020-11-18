9775029565c427dcdf748a5ce11919805fff28a0
RustyRin <ggk494@gmail.com>
2020-11-17 22:48:32 -0500

    test

--- polComp/questions.js
+++ polComp/questions.js
@@ -4,14 +4,14 @@ questions =  [
         "question": "Freedom of business is the best practical way a society can prosper.", 
         "effects": {
             "strongly_agree": {
-                "economic": 100,
-                "power": 100,
-                "social": 100
-            },
-            "agree": {
                 "economic": 10,
                 "power": 10,
                 "social": 10
+            },
+            "agree": {
+                "economic": 5,
+                "power": 5,
+                "social": 5
             },
             "neutral": {
                 "economic": 0,
@@ -19,14 +19,14 @@ questions =  [
                 "social": 0
             },
             "disagree": {
+                "economic": -5,
+                "power": -5,
+                "social": -5
+            },
+            "strongly_disagree": {
                 "economic": -10,
                 "power": -10,
                 "social": -10
-            },
-            "strongly_disagree": {
-                "economic": -100,
-                "power": -100,
-                "social": -100
             }
         }
     }
